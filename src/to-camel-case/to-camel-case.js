869c9e64ff1904e86b748e7ffed93a7fad14b089
MightyCupcakes <13433484+MightyCupcakes@users.noreply.github.com>
2023-04-28 17:58:34 +0800

    Implement to camel case kata

---
+++ src/to-camel-case/to-camel-case.js
@@ -0,0 +1,9 @@
+function toCamelCase(str){
+    return [...str.split(/[_-]/)]
+        .map( (v, i) => i === 0 ? v : v.substring(0, 1).toUpperCase() + v.substring(1))
+        .reduce( (prev , curr) => prev + curr, '')
+}
+
+console.log(toCamelCase('the-stealth-warrior'))
+console.log(toCamelCase('The_Stealth_Warrior'))
+console.log(toCamelCase('The_Stealth-Warrior'))
